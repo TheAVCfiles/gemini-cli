37d172e364c8c3e09c619c3ca0928eb6b845b192
A. C. Van Cura <166961554+TheAVCfiles@users.noreply.github.com>
2025-12-29 23:46:01 -0500

    Ensure tests resolve core package without build

---
+++ packages/cli/src/generated/git-commit.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This file is checked into source control to provide default metadata for
+// local development and testing. It is overwritten by
+// scripts/generate-git-commit-info.js during release builds.
+export const GIT_COMMIT_INFO = 'N/A';
+export const CLI_VERSION = 'UNKNOWN';
